b45530bb417ee6f0083c6691231ef0a378dfa2eb
konlanmikpekoah.km@outlook.com <konlanmikpekoah.km@outlook.com>
2021-01-05 13:19:41 +0000

    this is the image with the the circle avartar

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/monkey.jpg'),
+              ),
+              Text(
+                'Adam Grant',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
